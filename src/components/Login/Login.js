a02ac95eb2e1903b3036c54594d5f7edb5b9cc61
mamun8893 <0316361936@student.wub.edu.bd>
2021-10-18 20:00:50 +0600

    google signin integration

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "./login.css";
+import googleImg from "../../images/google.png";
 import { Button, Col, Container, Row, Tab, Tabs } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import useAuth from "../../hooks/useAuth";
 
 const Login = () => {
-  const { handleSignin, handleSignup } = useAuth();
+  const { handleSignin, handleSignup, handleGoogleSignin } = useAuth();
   const [key, setKey] = useState("login");
   const {
     register,
@@ -19,7 +20,6 @@ const Login = () => {
   } = useForm();
   const onSubmit = (data) => {
     handleSignin(data.email, data.password);
-    console.log(data);
   };
 
   const onSubmitSignup = (data) => {
@@ -31,79 +31,101 @@ const Login = () => {
       <Container>
         <Row className="justify-content-center">
           <Col md={5}>
-            <Tabs
-              id="controlled-tab-example"
-              activeKey={key}
-              onSelect={(k) => setKey(k)}
-              className="mb-3"
-            >
-              {/* Login Toggle */}
+            <div className="login-signup-card">
+              <Tabs
+                id="controlled-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}
+                className="mb-3"
+              >
+                {/* Login Toggle */}
 
-              <Tab eventKey="login" title="Login">
-                <div className="login-form">
-                  <form key={1} onSubmit={handleSubmit(onSubmit)}>
-                    <input
-                      type="email"
-                      {...register("email", { required: true })}
-                    />
-                    <input
-                      type="password"
-                      {...register("password", { required: true })}
-                    />
-                    <input
-                      type="submit"
-                      value="Log in"
-                      className="login-signup-btn"
-                    />
-                  </form>
-                </div>
-              </Tab>
+                <Tab eventKey="login" title="Login">
+                  <div className="login-form">
+                    <form key={1} onSubmit={handleSubmit(onSubmit)}>
+                      <input
+                        type="email"
+                        {...register("email", { required: true })}
+                      />
+                      <input
+                        type="password"
+                        {...register("password", { required: true })}
+                      />
+                      <input
+                        type="submit"
+                        value="Log in"
+                        className="login-signup-btn"
+                      />
+                      <div className="or-login-txt">
+                        <p>OR</p>
+                      </div>
+                      <div
+                        className="google-signup-btn"
+                        onClick={handleGoogleSignin}
+                      >
+                        <img src={googleImg} alt="" />
+                        <span>Sign in With Google </span>
+                      </div>
+                    </form>
+                  </div>
+                </Tab>
 
-              {/* SignUp Toggle */}
+                {/* SignUp Toggle */}
 
-              <Tab eventKey="register" title="Resister">
-                <div className="signup-form">
-                  <form key={2} onSubmit={handleSubmit2(onSubmitSignup)}>
-                    <input
-                      type="text"
-                      {...register2("name")}
-                      placeholder="Enter Your Name"
-                    />
-                    <input
-                      type="email"
-                      {...register2("email", { required: true })}
-                      placeholder="Enter Your Email"
-                    />
-                    {errors.email && (
-                      <span className="pb-2 d-block text-danger">
-                        This field is required
-                      </span>
-                    )}
-                    <input
-                      type="password"
-                      {...register2("password", { required: true })}
-                      placeholder="Enter Your Password"
-                    />
-                    {errors.password && (
-                      <span className="pb-2 d-block text-danger">
-                        This field is required
-                      </span>
-                    )}
+                <Tab eventKey="register" title="Resister">
+                  <div className="signup-form">
+                    <form key={2} onSubmit={handleSubmit2(onSubmitSignup)}>
+                      <input
+                        type="text"
+                        {...register2("name")}
+                        placeholder="Enter Your Name"
+                      />
+                      <input
+                        type="email"
+                        {...register2("email", { required: true })}
+                        placeholder="Enter Your Email"
+                      />
+                      {errors.email && (
+                        <span className="pb-2 d-block text-danger">
+                          This field is required
+                        </span>
+                      )}
+                      <input
+                        type="password"
+                        {...register2("password", { required: true })}
+                        placeholder="Enter Your Password"
+                      />
+                      {errors.password && (
+                        <span className="pb-2 d-block text-danger">
+                          This field is required
+                        </span>
+                      )}
 
-                    {errors.conformPassword && (
-                      <span className="pb-2 d-block text-danger">
-                        This field is required
-                      </span>
-                    )}
-                    <input
-                      type="submit"
-                      className="login-signup-btn"
-                      value="Sign up"
-                    />
-                  </form>
-                </div>
-              </Tab>
-            </Tabs>
+                      {errors.conformPassword && (
+                        <span className="pb-2 d-block text-danger">
+                          This field is required
+                        </span>
+                      )}
+                      <input
+                        type="submit"
+                        className="login-signup-btn"
+                        value="Sign up"
+                      />
+                      <div className="or-login-txt">
+                        <p>OR</p>
+                      </div>
+                      <div
+                        className="google-signup-btn"
+                        onClick={handleGoogleSignin}
+                      >
+                        <img src={googleImg} alt="" />
+                        <span>Sign in With Google </span>
+                      </div>
+                    </form>
+                  </div>
+                </Tab>
+              </Tabs>
+            </div>
           </Col>
         </Row>
       </Container>
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -4,6 +4,8 @@ import swal from "sweetalert";
 import initializeAuth from "../firebase/firebase.init";
 import {
   getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   onAuthStateChanged,
@@ -15,6 +17,7 @@ initializeAuth();
 const useFirebase = () => {
   const [user, setUser] = useState({});
   const [error, setError] = useState("");
+  const googleProvider = new GoogleAuthProvider();
   const auth = getAuth();
 
   useEffect(() => {
@@ -39,20 +42,30 @@ const useFirebase = () => {
         });
       })
       .catch((error) => {
-        switch (error.code) {
-          case "auth/email-already-in-use":
-            setError("Email Alredy Used");
-            break;
-          default:
-        }
+        swal("Something went wrong!", `${error.message}`, "error");
       });
   };
   const handleSignin = (email, password) => {
-    signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
-      const user = userCredential.user;
-      setUser(user);
-      swal("Good Job!", "Login Successfully", "success");
-    });
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setUser(user);
+        swal("Good Job!", "Login Successfully", "success");
+      })
+      .catch((error) => {
+        swal("Something went wrong!", `${error.message}`, "error");
+      });
+  };
+
+  const handleGoogleSignin = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        setUser(result.user);
+        swal("Good Job!", "Login Successfully", "success");
+      })
+      .catch((error) => {
+        swal("Something went wrong!", `${error.message}`, "error");
+      });
   };
 
   const handleLogout = () => {
@@ -61,7 +74,14 @@ const useFirebase = () => {
     });
   };
 
-  return { handleSignin, handleSignup, handleLogout, user, error };
+  return {
+    handleSignin,
+    handleSignup,
+    handleGoogleSignin,
+    handleLogout,
+    user,
+    error,
+  };
 };
 
 export default useFirebase;
